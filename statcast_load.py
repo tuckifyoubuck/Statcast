990421e5f0b16987361cfb81eaaec50f82860e8a
tuckifyoubuck <aaugtu@gmail.com>
2023-03-18 20:41:48 -0400

    Removed bulk function that was just inputing into PSQL method. Added additional code that querys new statcast db for unique pitchers and hitters and then does a reverse lookup for their names. This is then used to create a dimension table in the newly created statcast database. This will likely have to be cleaned up a bit and may have to be adjusted to include fielders at some point.

--- statcast_load.py
+++ statcast_load.py
@@ -1,10 +1,11 @@
 import pandas as pd
-from pybaseball import statcast
+from pybaseball import statcast, playerid_reverse_lookup
 from datetime import date
 import calendar
 from postgresql_class import PSQL
 import random
 import sys
+from sqlalchemy import text
 
 
 start = date(2008, 1, 1)
@@ -47,14 +48,11 @@ def create_tbl_from_csv_sample(filename, tbl_name, psql):
     temp_df = pd.read_csv(filename, skiprows=skip, low_memory=False)
     try:
         psql.create_tbl(dataframe=temp_df, tbl_name=tbl_name)
+        print('table created from random sample')
     except Exception as e:
         print('error: ', e)
         print('exception type: ', type(e))
         sys.exit(1)
-
-
-def bulk_insert_csv_into_tbl(csv, tbl_name, psql):
-    psql.bulk_insert_into_tbl_from_csv(filename=csv, tbl_name=tbl_name)
 
 
 if __name__ == "__main__":
@@ -71,8 +69,21 @@ if __name__ == "__main__":
     # this is to prevent creating the table based on old data with empty columns or new data with empty columns
     new_db = PSQL(user=user, password=pw, database=statcast_db_name)
     create_tbl_from_csv_sample(filename=csv_file_name, tbl_name=statcast_tbl_name, psql=new_db)
-    print('table created from random sample')
 
     # bulk insert csv into fresh table
-    bulk_insert_csv_into_tbl(csv=csv_file_name, tbl_name=statcast_tbl_name, psql=new_db)
+    new_db.bulk_insert_into_tbl_from_csv(csv=csv_file_name, tbl_name=statcast_tbl_name)
+
+    # query statcast DB for distinct pitcher/batter ids
+    # then does a reverse lookup using the statcast package for player names
+    player_query = """
+    select distinct pitcher as player from public.statcast_pitch sp 
+    union
+    select distinct batter as player from public.statcast_pitch sp
+    """
+    ids_df = pd.read_sql(sql=text(player_query), con=new_db.engine.connect())
+    ids_list = ids_df['player'].values.tolist()
+    final_df = playerid_reverse_lookup(ids_list)
+    new_db.create_tbl(final_df, 'players')
+    new_db.insert_into_tbl(final_df, 'players')
+
 
